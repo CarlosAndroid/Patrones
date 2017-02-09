c329c48bd46ffac2038f85edc110e4b4e76275e5
Carlos Sanchidrian <carlos90_san@hotmail.com>
2017-02-09 03:41:28 +0100

    Añadido botón

--- src/main/java/com/example/ramiro/patrones/singleton.java
+++ src/main/java/com/example/ramiro/patrones/singleton.java
@@ -2,6 +2,7 @@ package com.example.ramiro.patrones;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class singleton extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class singleton extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singleton);
+
+        Button button;
     }
 }
