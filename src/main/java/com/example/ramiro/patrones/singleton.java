846f507e468c29adb01310ada47e91bd632704a1
Carlos Sanchidrian <carlos90_san@hotmail.com>
2017-02-09 03:38:38 +0100

    Initial commit

---
+++ src/main/java/com/example/ramiro/patrones/singleton.java
@@ -0,0 +1,13 @@
+package com.example.ramiro.patrones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class singleton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singleton);
+    }
+}
